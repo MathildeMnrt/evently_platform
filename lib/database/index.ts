26f2caf9d9387b7faa60e5ee1bc3d5765f417cd8
Mathilde <mathilde@Mathildes-MacBook-Pro.local>
2024-03-12 01:03:53 +0100

    first commit

---
+++ lib/database/index.ts
@@ -0,0 +1,24 @@
+import mongoose, { mongo } from 'mongoose';
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+// we initialize a cache variable intend to hold the cached connexion to our db
+let cached = (global as any).mongoose || { conn : null, promise : null };
+
+
+export const connectToDatabase = async () => {
+    if (cached.conn) return cached.conn;
+
+    if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
+
+    // we either connect to an already cached connection or we create a new one
+    cached.promise = cached.promise || mongoose.connect(MONGODB_URI,{
+        dbName: 'evently',
+        bufferCommands: false,
+    })
+
+    cached.conn = await cached.promise;
+}
+
+// Server actions
+// connectToDatabase() ...
